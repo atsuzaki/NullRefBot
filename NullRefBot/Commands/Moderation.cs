a29d8b88312b3b32e521f25b6d4c50c033752bb3
Katherine Philip <kurerunoshikaku@yahoo.com>
2018-10-21 10:12:20 +0700

    WIP stuff

--- NullRefBot/Commands/Moderation.cs
+++ NullRefBot/Commands/Moderation.cs
@@ -5,20 +5,30 @@ using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using NullRefBot.Utils;
+using RestSharp;
 
 namespace NullRefBot.Commands
 {
 	public class Moderation : BaseCommandModule
 	{
+        //TODO: figure out how to restrict to admin/mods/owner only
 		[Command("mute")]
 		public async Task Mute(CommandContext ctx, DiscordMember member, int duration = 5) {
 		    const ulong MUTED_ROLE_ID = 503356983353802752; //TEMP
 
 			await ctx.TriggerTypingAsync();
-			await ctx.RespondAsync($"*{member.DisplayName}* is muted for {duration} minutes");
+			await ctx.RespondAsync($"**{member.DisplayName}** is muted for {duration} minutes");
 
-		    await member.GrantRoleAsync(ctx.Guild.GetRole(MUTED_ROLE_ID));
-            //set timeout to check db later
+		    int durationInMs = duration * 1000 * 60;
+		    Helpers.SetTimeout(() => {
+		        PostMute(member.Id, -1);
+		        member.RevokeRoleAsync(ctx.Guild.GetRole(MUTED_ROLE_ID));
+		    }, durationInMs);
+
+            await member.GrantRoleAsync(ctx.Guild.GetRole(MUTED_ROLE_ID));
+
+		    //check db later
 		}
 
 		[Command("unmute")]
@@ -26,10 +36,26 @@ namespace NullRefBot.Commands
 		    const ulong MUTED_ROLE_ID = 503356983353802752; //TEMP
 
 			await ctx.TriggerTypingAsync();
-			await ctx.RespondAsync($"*{member.DisplayName}* is unmuted");
+			await ctx.RespondAsync($"**{member.DisplayName}** is unmuted");
 
+		    PostMute(member.Id, -1);
 		    await member.RevokeRoleAsync(ctx.Guild.GetRole(MUTED_ROLE_ID));
-            //reset db
 		}
+
+	    private async Task<bool> TryUnmuteAsync(DiscordMember member) {
+            //ask permission to unmute 
+            PostMute(member.Id, -1);
+	        return true;
+	    }
+
+	    private void PostMute(ulong discordID, int durationInSeconds) {
+            var req = new RestRequest();
+            req.Resource = "/mute/{discord_id}?mute_time={amount}"; //TODO: temp
+            req.AddParameter("discord_id", discordID);
+            req.AddParameter("amount", durationInSeconds);
+
+            req.Method = Method.PUT;
+            req.RequestFormat = DataFormat.Json;
+        }
 	}
 }
---
+++ NullRefBot/Utils/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NullRefBot.Utils {
+    public class Helpers {
+        public static void SetTimeout(Action callback, int delayInMs) {
+            var timer = new System.Timers.Timer(delayInMs);
+            timer.Elapsed += (source, e) => {
+                callback.Invoke();
+
+                timer.Stop();
+                timer.Dispose();
+            };
+
+            timer.Enabled = true;
+            timer.Start();
+        }
+    }
+}
