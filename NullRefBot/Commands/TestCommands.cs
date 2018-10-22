6037864f980c3dcb5588ee96001837d90f3bff22
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2018-10-22 09:40:33 +0200

    ron swanson quotes

--- NullRefBot/Commands/TestCommands.cs
+++ NullRefBot/Commands/TestCommands.cs
@@ -10,8 +10,30 @@ using RestSharp;
 
 namespace NullRefBot.Commands
 {
-	public class TestCommands : BaseCommandModule
-	{
+  public class TestCommands : BaseCommandModule {
+
+    static String[] RonSwansonQuotes = new[] {
+      "You had me at \"Meat Tornado\".",
+      "There's only one thing I hate more than lying: skim milk. Which is water that is lying about being milk.",
+      "I'd wish you the best of luck but I believe luck is a concept created by the weak to explain their failures.",
+      "Dear frozen yogurt, you are the celery of desserts. Be ice cream or be nothing. Zero stars.",
+      "I'm not interested in caring about people.",
+      "Crying: Acceptable at funerals and the Grand Canyon.",
+      "There are three acceptable haircuts: high and tight, crew cut, buzz cut.",
+      "Normally, if given the choice between doing something and nothing, I’d choose to do nothing. But I will do something if it helps someone else do nothing. I’d work all night, if it meant nothing got done.",
+      "Under my tutelage, you will grow from boys to men. From men into gladiators. And from gladiators into Swansons.",
+      "It’s pointless for a human to paint scenes of nature when they can go outside and stand in it.",
+      "I once worked with a guy for three years and never learned his name. Best friend I ever had. We still never talk sometimes.",
+      "Fishing relaxes me. It’s like yoga, except I still get to kill something.",
+      "History began on July 4, 1776. Everything that happened before that was a mistake.",
+      "Give a man a fish and feed him for a day. Don’t teach a man to fish…and feed yourself. He’s a grown man. And fishing’s not that hard.",
+      "Sting like a bee. Do not float like a butterfly. That's ridiculous.",
+      "Give 100%. 110% is impossible. Only idiots recommend that.",
+      "Capitalism: God's way of determining who is smart and who is poor.",
+      "Fishing is for sport only. Fish meat is practically a vegetable.",
+      "Any dog under fifty pounds is a cat and cats are useless.",
+    };
+
 		[Command("ping")]
 		public async Task Ping(CommandContext ctx)
 		{
@@ -41,6 +63,21 @@ namespace NullRefBot.Commands
       DiscordEmbedBuilder embed;
       embed = new DiscordEmbedBuilder();
       embed.ImageUrl = "https://media1.tenor.com/images/66b9e27c779a1a314f0a8b31bb5609f7/tenor.gif";
+
+      await ctx.RespondAsync(embed: embed);
+    }
+
+    [Command("swanson")]
+    public async Task Ron(CommandContext ctx) {
+      await ctx.TriggerTypingAsync();
+
+      DiscordEmbedBuilder embed;
+      embed = new DiscordEmbedBuilder();
+      embed.Author = new DiscordEmbedBuilder.EmbedAuthor();
+      embed.Description = RonSwansonQuotes[Environment.TickCount % RonSwansonQuotes.Length];
+      embed.ThumbnailUrl = "https://uproxx.files.wordpress.com/2016/06/ron-swanson-feature.jpg?quality=95&w=650&h=360";
+      embed.Footer = new DiscordEmbedBuilder.EmbedFooter();
+      embed.Footer.Text = " - Ron Swanson";
 
       await ctx.RespondAsync(embed: embed);
     }
