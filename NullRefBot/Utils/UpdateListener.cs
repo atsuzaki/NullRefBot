77d1cf0f5b655359b705fc9a4b13f8e2015f5413
Death <DeathGameDev@gmail.com>
2018-10-22 22:00:44 -0500

    Narrowed issue down to invalid color

--- NullRefBot/Utils/UpdateListener.cs
+++ NullRefBot/Utils/UpdateListener.cs
@@ -11,24 +11,15 @@ namespace NullRefBot.Utils
 	public static class UpdateListener
 	{
 		private const ulong TRAVIS_CHANNEL = 504110403467476994;
-		private const int SUCCESS_COLOR = 3066993;
+		private static DiscordColor successColor = new DiscordColor("#2ECC71");
 
 		public static void CheckMessage(MessageCreateEventArgs e)
 		{
-			Console.WriteLine("CheckMessage called. channel id = " + e.Channel.Id);
 			if (e.Channel.Id != TRAVIS_CHANNEL)
-			{
-				Console.WriteLine("Returning due to invalid channel id. Required ID: " + TRAVIS_CHANNEL + " actual ID: " + e.Channel.Id);
 				return;
-			}
 
-			if (e.Message.Embeds.Count == 0 || e.Message.Embeds[0].Color != SUCCESS_COLOR)
-			{
-				Console.WriteLine("Returning due to invalid embeds. Count = " + e.Message.Embeds.Count);
-				if (e.Message.Embeds.Count > 0)
-					Console.WriteLine("\tInvalid color: " + e.Message.Embeds[0].Color + " Required color: " + SUCCESS_COLOR);
+			if (e.Message.Embeds.Count == 0 || e.Message.Embeds[0].Color != successColor)
 				return;
-			}
 
 			Bot.Instance.Client.UpdateStatusAsync(new DiscordActivity("with Continous Integration", ActivityType.Playing));
 			Bot.Instance.Client.DebugLogger.LogMessage(LogLevel.Info, "CI", "Updating bot", DateTime.Now);
